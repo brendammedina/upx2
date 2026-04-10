3e58c3d1974c09d4cc438e1f4d6ee9e21514ec82
brendammedina <71186164+brendammedina@users.noreply.github.com>
2026-04-09 20:31:15 -0400

    Create database and usuario table in banco.sql

---
+++ banco.sql
@@ -0,0 +1,9 @@
+
+CREATE DATABASE crud_spring;
+USE crud_spring;
+
+CREATE TABLE usuario (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE
+);
